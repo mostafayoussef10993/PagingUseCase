21c5347e667b77f67cae7dc3ce41ce2811f37430
mostafayoussef10993 <mostafayoussef10993@gmail.com>
2026-03-01 22:33:30 +0200

    Making user model nullable

--- lib/login/models/user.dart
+++ lib/login/models/user.dart
@@ -1,72 +1,76 @@
 class User {
-  final int id;
-  final int type;
-  final int active;
-  final String name;
-  final String username;
-  final String contactPerson;
-  final String email;
-  final String phone;
-  final String code;
-  final double balance;
+  final int? id;
+  final int? type;
+  final int? active;
+  final String? name;
+  final String? username;
+  final String? contactPerson;
+  final String? email;
+  final String? phone;
+  final String? code;
+  final double? balance;
   final int? sellerId;
-  final int commercialNum;
+  final int? commercialNum;
   final int? nationalId;
   final int? licenseNum;
-  final int locationId;
-  final List<int> customerGroupsIds;
-  final int walletId;
-  final int loginMethod;
-  final int cartItemsCount;
-  final int unreadNotificationsCount;
+  final int? locationId;
+  final List<int>? customerGroupsIds;
+  final int? walletId;
+  final int? loginMethod;
+  final int? cartItemsCount;
+  final int? unreadNotificationsCount;
 
   User({
-    required this.id,
-    required this.type,
-    required this.active,
-    required this.name,
-    required this.username,
-    required this.contactPerson,
-    required this.email,
-    required this.phone,
-    required this.code,
-    required this.balance,
-    required this.sellerId,
-    required this.commercialNum,
-    required this.nationalId,
-    required this.licenseNum,
-    required this.locationId,
-    required this.customerGroupsIds,
-    required this.walletId,
-    required this.loginMethod,
-    required this.cartItemsCount,
-    required this.unreadNotificationsCount,
+    this.id,
+    this.type,
+    this.active,
+    this.name,
+    this.username,
+    this.contactPerson,
+    this.email,
+    this.phone,
+    this.code,
+    this.balance,
+    this.sellerId,
+    this.commercialNum,
+    this.nationalId,
+    this.licenseNum,
+    this.locationId,
+    this.customerGroupsIds,
+    this.walletId,
+    this.loginMethod,
+    this.cartItemsCount,
+    this.unreadNotificationsCount,
   });
 
   factory User.fromJson(Map<String, dynamic> json) {
     return User(
-      id: json['id'],
-      type: json['type'],
-      active: json['active'],
-      name: json['name'],
-
-      username: json['username'],
-      contactPerson: json['contact_person'],
-      email: json['email'],
-      phone: json['phone'],
-      code: json['code'],
-      balance: (json['balance'] as num).toDouble(),
-      sellerId: json['seller_id'],
-      commercialNum: json['commercial_num'],
-      nationalId: json['national_id'],
-      licenseNum: json['license_num'],
-      locationId: json['location_id'],
-      customerGroupsIds: List<int>.from(json['customer_groups_ids']),
-      walletId: json['wallet_id'],
-      loginMethod: json['login_method'],
-
-      cartItemsCount: json['cart_items_count'],
-      unreadNotificationsCount: json['unread_notifications_count'],
+      id: json['id'] as int?,
+      type: json['type'] as int?,
+      active: json['active'] as int?,
+      name: json['name'] as String?,
+      username: json['username'] as String?,
+      contactPerson: json['contact_person'] as String?,
+      email: json['email'] as String?,
+      phone: json['phone'] as String?,
+      code: json['code'] as String?,
+      balance: (json['balance'] as num?)?.toDouble(),
+      sellerId: json['seller_id'] as int?,
+      commercialNum: json['commercial_num'] as int?,
+      nationalId: json['national_id'] as int?,
+      licenseNum: json['license_num'] as int?,
+      locationId: json['location_id'] as int?,
+      customerGroupsIds: json['customer_groups_ids'] != null
+          ? List<int>.from(
+              (json['customer_groups_ids'] as List<dynamic>).map(
+                (e) => (e as num).toInt(),
+              ),
+            )
+          : null,
+      walletId: json['wallet_id'] as int?,
+      loginMethod: json['login_method'] as int?,
+      cartItemsCount: json['cart_items_count'] as int?,
+      unreadNotificationsCount: json['unread_notifications_count'] as int?,
     );
   }
 }
