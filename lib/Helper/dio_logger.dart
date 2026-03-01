e9ef0226fa6a92f88d7c3eeb803cee7369635ecd
mostafayoussef10993 <mostafayoussef10993@gmail.com>
2026-03-01 22:42:37 +0200

    dio logger

--- lib/Helper/dio_logger.dart
+++ lib/Helper/dio_logger.dart
@@ -1,3 +1,22 @@
-// *** API START 
+// *** API START
+
+import 'package:dio/dio.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+
+// Function to set up Dio logger
+void setupDioLogger(Dio dio) {
+  dio.interceptors.add(
+    PrettyDioLogger(
+      request: true,
+      requestHeader: true,
+      requestBody: true,
+      responseBody: true,
+      responseHeader: false,
+      error: true,
+      compact: true,
+      maxWidth: 120,
+    ),
+  );
+}
 
 // *** API END
